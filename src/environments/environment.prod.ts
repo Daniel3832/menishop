75f762cf451e7da5de9e0b2119d4d94dbd863732
daniel3832 <daniel3832@gmail.com>
2018-10-15 23:00:58 -0700

    user auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDo56XaUYWAPAw_xG8bdLZ_iCoNPPkb03w",
+    authDomain: "menishop-88296.firebaseapp.com",
+    databaseURL: "https://menishop-88296.firebaseio.com",
+    projectId: "menishop-88296",
+    storageBucket: "menishop-88296.appspot.com",
+    messagingSenderId: "254926272701"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDo56XaUYWAPAw_xG8bdLZ_iCoNPPkb03w",
+    authDomain: "menishop-88296.firebaseapp.com",
+    databaseURL: "https://menishop-88296.firebaseio.com",
+    projectId: "menishop-88296",
+    storageBucket: "menishop-88296.appspot.com",
+    messagingSenderId: "254926272701"
+  }
 };
